7e68bcf523ad5f924d4acc8dabdf814d948a98d2
Dario Caruso <darioyzf@gmail.com>
2015-10-03 04:45:00 +0200

    oki

--- tests/main.js
+++ tests/main.js
@@ -1,4 +1,5 @@
-var timerdaemon=require('../index.js');
-var verb=require('verbo');
+var timerdaemon=require('../index.js'),
+verb=require('verbo');
+
 timerdaemon.pre(5555,function(){verb("completed after 5555","verbose","1test")});
 timerdaemon.post(5555,function(){verb("completed after 5555","debug","1test")});
