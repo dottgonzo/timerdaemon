924a2f14c9c7331fb373e7dfc80a5c66188eef4e
dcaruso@movia.biz <Dario Caruso>
2019-04-13 20:35:51 +0200

    ok finally

--- test/async.js
+++ test/async.js
@@ -3,12 +3,11 @@ const chai_1 = require("chai");
 const timerdaemon = require("../index");
 describe('async', () => {
     describe('pre Async', () => {
-        it('after 9.1 seconds post function was executed 10 times', (done) => {
+        it('after 8.5 seconds post function was executed 3 times', (done) => {
             let valpre = 0;
             const test = () => {
                 return new Promise((resolve, reject) => {
                     setTimeout(() => {
-                        console.log('pre' + valpre);
                         valpre++;
                         resolve();
                     }, 2000);
@@ -18,16 +17,15 @@ describe('async', () => {
             setTimeout(() => {
                 chai_1.expect(valpre).to.be.deep.equal(3);
                 done();
-            }, 9100);
+            }, 8500);
         });
     });
     describe('post Async', () => {
-        it('after 9.9 seconds post function was executed 9 times', (done) => {
+        it('after 8.5 seconds post function was executed 2 times', (done) => {
             let valpost = 0;
             const test = () => {
                 return new Promise((resolve, reject) => {
                     setTimeout(() => {
-                        console.log('post' + valpost);
                         valpost++;
                         resolve();
                     }, 2000);
@@ -35,9 +33,9 @@ describe('async', () => {
             };
             timerdaemon.post(test, 1000);
             setTimeout(() => {
-                chai_1.expect(valpost).to.be.deep.equal(3);
+                chai_1.expect(valpost).to.be.deep.equal(2);
                 done();
-            }, 9900);
+            }, 8500);
         });
     });
 });
--- test/async.ts
+++ test/async.ts
@@ -5,12 +5,11 @@ import * as timerdaemon from '../index'
 describe('async', () => {
 
   describe('pre Async', () => {
-    it('after 9.1 seconds post function was executed 10 times', (done) => {
+    it('after 8.5 seconds post function was executed 3 times', (done) => {
       let valpre = 0
       const test = () => {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
-            console.log('pre' + valpre)
             valpre++
             resolve()
 
@@ -21,17 +20,16 @@ describe('async', () => {
       setTimeout(() => {
         expect(valpre).to.be.deep.equal(3)
         done()
-      }, 9100)
+      }, 8500)
     })
   })
 
   describe('post Async', () => {
-    it('after 9.9 seconds post function was executed 9 times', (done) => {
+    it('after 8.5 seconds post function was executed 2 times', (done) => {
       let valpost = 0
       const test = () => {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
-            console.log('post' + valpost)
             valpost++
             resolve()
 
@@ -40,9 +38,9 @@ describe('async', () => {
       }
       timerdaemon.post(test, 1000)
       setTimeout(() => {
-        expect(valpost).to.be.deep.equal(3)
+        expect(valpost).to.be.deep.equal(2)
         done()
-      }, 9900)
+      }, 8500)
     })
   })
 
